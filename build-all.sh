0b3e4e60da739d8d55873681b0ebe04b966a4cf7
KoaLaYT <hytohjen@gmail.com>
2024-06-17 12:20:50 +0800

    init: setup for macos

---
+++ build-all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Build script for rebuilding everything
+set echo on
+
+echo "Building everything..."
+
+
+pushd engine
+source build.sh
+
+ERRORLEVEL=$?
+if [ $ERRORLEVEL -ne 0 ]
+then
+echo "Error:"$ERRORLEVEL && exit
+fi
+
+popd
+
+pushd testbed
+source build.sh
+ERRORLEVEL=$?
+if [ $ERRORLEVEL -ne 0 ]
+then
+echo "Error:"$ERRORLEVEL && exit
+fi
+
+popd
+
+echo "All assemblies built successfully."
---
+++ engine/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Build script for engine
+set echo on
+
+mkdir -p ../bin
+
+# Get a list of all the .c files.
+cFilenames=$(find . -type f -name "*.c")
+
+# echo "Files:" $cFilenames
+
+assembly="engine"
+compilerFlags="-g -shared -fdeclspec -fPIC -dynamiclib -install_name @rpath/bin/lib$assembly.dylib"
+# -fms-extensions 
+# -Wall -Werror
+includeFlags="-Isrc -I$VULKAN_SDK/include"
+linkerFlags="-lvulkan -L$VULKAN_SDK/lib"
+defines="-D_DEBUG -DKEXPORT"
+
+echo "Building $assembly..."
+echo clang $cFilenames $compilerFlags -o ../bin/lib$assembly.dylib $defines $includeFlags $linkerFlags
+clang $cFilenames $compilerFlags -o ../bin/lib$assembly.dylib $defines $includeFlags $linkerFlags

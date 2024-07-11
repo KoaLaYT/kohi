3d931234f108c8c39d316cb798beb4273cce2387
KoaLaYT <hytohjen@gmail.com>
2024-07-11 17:12:49 +0800

    feat(render): exts, layers and debugger

--- engine/src/renderer/vulkan/vulkan_backend.c
+++ engine/src/renderer/vulkan/vulkan_backend.c
@@ -2,11 +2,22 @@
 
 #include "containers/darray.h"
 #include "core/kmemory.h"
+#include "core/kstring.h"
 #include "core/logger.h"
 #include "vulkan_types.h"
 
 // static Vulkan context
 static vulkan_context context;
+
+VKAPI_ATTR VkBool32 VKAPI_CALL vk_debug_callback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+    VkDebugUtilsMessageTypeFlagsEXT message_types,
+    const VkDebugUtilsMessengerCallbackDataEXT* callback_data, void* user_data);
+
+/**
+ * The names of required extensions for this platform.
+ */
+const char** platform_get_required_extension_names();
 
 b8 vulkan_renderer_backend_initialize(renderer_backend* backend,
                                       const char* application_name,
@@ -26,30 +37,121 @@ b8 vulkan_renderer_backend_initialize(renderer_backend* backend,
     VkInstanceCreateInfo create_info = {VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO};
     create_info.pApplicationInfo = &app_info;
 #ifdef KPLATFORM_APPLE
-    const char** ext_names = darray_create(const char*);
-    darray_push(ext_names, &VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
     create_info.flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+#endif
+
+    const char** ext_names = platform_get_required_extension_names();
+#if defined(_DEBUG)
+    darray_push(ext_names,
+                &VK_EXT_DEBUG_UTILS_EXTENSION_NAME);  // debug utilities
+
+    KDEBUG("Required extensions:");
+    u32 length = darray_length(ext_names);
+    for (u32 i = 0; i < length; ++i) {
+        KDEBUG(ext_names[i]);
+    }
+#endif
     create_info.enabledExtensionCount = darray_length(ext_names);
     create_info.ppEnabledExtensionNames = ext_names;
-#elif
-    create_info.enabledExtensionCount = 0;
-    create_info.ppEnabledExtensionNames = 0;
-#endif
-    create_info.enabledLayerCount = 0;
-    create_info.ppEnabledLayerNames = 0;
 
-    VkResult result =
-        vkCreateInstance(&create_info, context.allocator, &context.instance);
-    if (result != VK_SUCCESS) {
-        KERROR("vkCreateInstance failed with result: %d", result);
-        return FALSE;
+    // Validation layers.
+    const char** required_validation_layer_names = 0;
+    u32 required_validation_layer_count = 0;
+
+    // If validation should be done, get a list of the required validation
+    // layert names and make sure they exist. Validation layers should only be
+    // enabled on non-release builds.
+#if defined(_DEBUG)
+    KINFO("Validation layers enabled. Enumerating...");
+
+    // The list of validation layers required.
+    required_validation_layer_names = darray_create(const char*);
+    darray_push(required_validation_layer_names,
+                &"VK_LAYER_KHRONOS_validation");
+    required_validation_layer_count =
+        darray_length(required_validation_layer_names);
+
+    // Obtain a list of available validation layers
+    u32 available_layer_count = 0;
+    VK_CHECK(vkEnumerateInstanceLayerProperties(&available_layer_count, 0));
+    VkLayerProperties* available_layers =
+        darray_reserve(VkLayerProperties, available_layer_count);
+    VK_CHECK(vkEnumerateInstanceLayerProperties(&available_layer_count,
+                                                available_layers));
+
+    // Verify all required layers are available.
+    for (u32 i = 0; i < required_validation_layer_count; ++i) {
+        KINFO("Searching for layer: %s...", required_validation_layer_names[i]);
+        b8 found = FALSE;
+        for (u32 j = 0; j < available_layer_count; ++j) {
+            if (strings_equal(required_validation_layer_names[i],
+                              available_layers[j].layerName)) {
+                found = TRUE;
+                KINFO("Found.");
+                break;
+            }
+        }
+
+        if (!found) {
+            KFATAL("Required validation layer is missing: %s",
+                   required_validation_layer_names[i]);
+            return FALSE;
+        }
     }
+    KINFO("All required validation layers are present.");
+#endif
+
+    create_info.enabledLayerCount = required_validation_layer_count;
+    create_info.ppEnabledLayerNames = required_validation_layer_names;
+
+    VK_CHECK(
+        vkCreateInstance(&create_info, context.allocator, &context.instance));
+    KINFO("Vulkan Instance created.");
+
+    // Debugger
+#if defined(_DEBUG)
+    KDEBUG("Creating Vulkan debugger...");
+    u32 log_severity =
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT;  //|
+                                                       //    VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT;
+
+    VkDebugUtilsMessengerCreateInfoEXT debug_create_info = {
+        VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT};
+    debug_create_info.messageSeverity = log_severity;
+    debug_create_info.messageType =
+        VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT;
+    debug_create_info.pfnUserCallback = vk_debug_callback;
+
+    PFN_vkCreateDebugUtilsMessengerEXT func =
+        (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+            context.instance, "vkCreateDebugUtilsMessengerEXT");
+    KASSERT_MSG(func, "Failed to create debug messenger!");
+    VK_CHECK(func(context.instance, &debug_create_info, context.allocator,
+                  &context.debug_messenger));
+    KDEBUG("Vulkan debugger created.");
+#endif
 
     KINFO("Vulkan renderer initialized successfully.");
     return TRUE;
 }
 
-void vulkan_renderer_backend_shutdown(renderer_backend* backend) {}
+void vulkan_renderer_backend_shutdown(renderer_backend* backend)
+{
+    KDEBUG("Destroying Vulkan debugger...");
+    if (context.debug_messenger) {
+        PFN_vkDestroyDebugUtilsMessengerEXT func =
+            (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+                context.instance, "vkDestroyDebugUtilsMessengerEXT");
+        func(context.instance, context.debug_messenger, context.allocator);
+    }
+
+    KDEBUG("Destroying Vulkan instance...");
+    vkDestroyInstance(context.instance, context.allocator);
+}
 
 void vulkan_renderer_backend_on_resized(renderer_backend* backend, u16 width,
                                         u16 height)
@@ -65,4 +167,40 @@ b8 vulkan_renderer_backend_begin_frame(renderer_backend* backend,
 b8 vulkan_renderer_backend_end_frame(renderer_backend* backend, f32 delta_time)
 {
     return TRUE;
+}
+
+const char** platform_get_required_extension_names()
+{
+    const char** ext_names = darray_create(const char*);
+    darray_push(ext_names, &VK_KHR_SURFACE_EXTENSION_NAME);
+
+#ifdef KPLATFORM_APPLE
+    darray_push(ext_names, &VK_EXT_METAL_SURFACE_EXTENSION_NAME);
+    darray_push(ext_names, &VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+#endif
+
+    return ext_names;
+}
+
+VKAPI_ATTR VkBool32 VKAPI_CALL vk_debug_callback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+    VkDebugUtilsMessageTypeFlagsEXT message_types,
+    const VkDebugUtilsMessengerCallbackDataEXT* callback_data, void* user_data)
+{
+    switch (message_severity) {
+        default:
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+            KERROR(callback_data->pMessage);
+            break;
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+            KWARN(callback_data->pMessage);
+            break;
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
+            KINFO(callback_data->pMessage);
+            break;
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
+            KTRACE(callback_data->pMessage);
+            break;
+    }
+    return VK_FALSE;
 }
--- engine/src/renderer/vulkan/vulkan_types.h
+++ engine/src/renderer/vulkan/vulkan_types.h
@@ -1,10 +1,24 @@
 #pragma once
 
-#include <vulkan/vulkan.h>
-
+#include "core/asserts.h"
 #include "defines.h"
+
+#ifdef KPLATFORM_APPLE
+#define VK_USE_PLATFORM_METAL_EXT
+#endif
+#include <vulkan/vulkan.h>
 
 typedef struct vulkan_context {
     VkInstance instance;
     VkAllocationCallbacks* allocator;
+
+#if defined(_DEBUG)
+    VkDebugUtilsMessengerEXT debug_messenger;
+#endif
 } vulkan_context;
+
+// Checks the given expression's return value against VK_SUCCESS.
+#define VK_CHECK(expr)               \
+    {                                \
+        KASSERT(expr == VK_SUCCESS); \
+    }
